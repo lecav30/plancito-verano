a86c2f8002b3101394ef56efa31794b72cc3e051
frouillon <favio.rouillon@unmsm.edu.pe>
2024-02-13 11:18:49 -0500

    contador no estático

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,5 +1,44 @@
+import {useState , useEffect} from 'react';
 const Countdown = () => {
-  return <div>Countdown!</div>;
+  const [dias, setDias] = useState(0);
+  const [horas, setHoras] = useState(0);
+  const [minutos, setMin] = useState(0);
+  const [segundos, setSeg] = useState(0);
+
+  useEffect(() => {
+    const fechaLimite = new Date('2024-03-09T00:00:00');
+    const actualizarContador = () =>{
+      const fechaActual = new Date();
+      const diferenciaTiempo = fechaLimite.getTime() - fechaActual.getTime();
+      const diferenciaDias = Math.ceil(diferenciaTiempo / (1000 * 60 * 60 *24));
+      const diferenciaHoras = Math.ceil((diferenciaTiempo % (1000 * 60 * 60 *24))/(1000*60*60));
+      const difereciaMin = Math.ceil((diferenciaTiempo % (1000*60*60)) / (1000*60));
+      const diferenciaSeg = Math.ceil((diferenciaTiempo % (1000*60)) / (1000));
+
+      setDias(diferenciaDias);
+      setHoras(diferenciaHoras);
+      setMin(difereciaMin);
+      setSeg(diferenciaSeg);
+    }
+    
+  
+    const reloj = setInterval(actualizarContador,1000);
+
+    return function limpieza (){
+      clearInterval(reloj);
+    }
+  });
+  return (
+    <div>
+      <h1>Countdown!</h1>
+      <p>Dias restantes: {dias}</p>
+      <p>Horas: {horas}</p>
+      <p>Minutos: {minutos}</p>
+      <p>Segundos: {segundos}</p>
+      
+    </div>
+
+  );
 };
 
 export default Countdown;
