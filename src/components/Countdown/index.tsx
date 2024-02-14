5df022cae6250f183d9681f3b65e2e327a14366e
lecav30 <lecav30@outlook.com>
2024-02-13 21:42:56 -0500

    english variables

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,11 +1,11 @@
 import { useRef, useState, useEffect } from "react";
-import Playa from "../../images/playahd2.jpg";
+import Beach from "../../images/playahd2.jpg";
 
 const Countdown = () => {
-  const [dias, setDias] = useState(0);
-  const [horas, setHoras] = useState(0);
-  const [minutos, setMin] = useState(0);
-  const [segundos, setSeg] = useState(0);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   const daysCircle = useRef<SVGCircleElement | null>(null);
   const hourCircle = useRef<SVGCircleElement | null>(null);
@@ -13,63 +13,62 @@ const Countdown = () => {
   const secondCircle = useRef<SVGCircleElement | null>(null);
 
   const actualizarContador = () => {
-    const fechaLimite = new Date("2024-03-09T00:00:00");
-    const fechaActual = new Date();
-    const diferenciaTiempo = fechaLimite.getTime() - fechaActual.getTime();
-    const diferenciaDias = Math.ceil(diferenciaTiempo / (1000 * 60 * 60 * 24));
-    const diferenciaHoras = Math.ceil(
-      (diferenciaTiempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    const deadline = new Date("2024-03-09T00:00:00");
+    const currentDate = new Date();
+    const timeDifference = deadline.getTime() - currentDate.getTime();
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursDifference = Math.ceil(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-    const difereciaMin = Math.ceil(
-      (diferenciaTiempo % (1000 * 60 * 60)) / (1000 * 60)
+    const minutesDifference = Math.ceil(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
     );
-    const diferenciaSeg = Math.ceil((diferenciaTiempo % (1000 * 60)) / 1000);
+    const secondsDifference = Math.ceil((timeDifference % (1000 * 60)) / 1000);
 
-    setDias(diferenciaDias);
-    setHoras(diferenciaHoras);
-    setMin(difereciaMin);
-    setSeg(diferenciaSeg);
+    setDays(daysDifference);
+    setHours(hoursDifference);
+    setMinutes(minutesDifference);
+    setSeconds(secondsDifference);
 
     if (daysCircle.current) {
       daysCircle.current.style.strokeDashoffset = `${
-        440 - (440 * diferenciaDias) / 365
+        440 - (440 * daysDifference) / 365
       }px`;
     }
     if (hourCircle.current) {
       hourCircle.current.style.strokeDashoffset = `${
-        451 - (451 * diferenciaHoras) / 24
+        451 - (451 * hoursDifference) / 24
       }px`;
     }
     if (minuteCircle.current) {
       minuteCircle.current.style.strokeDashoffset = `${
-        451 - (451 * difereciaMin) / 60
+        451 - (451 * minutesDifference) / 60
       }px`;
     }
     if (secondCircle.current) {
       secondCircle.current.style.strokeDashoffset = `${
-        451 - (451 * diferenciaSeg) / 60
+        451 - (451 * secondsDifference) / 60
       }px`;
     }
   };
 
   useEffect(() => {
-    const reloj = setInterval(actualizarContador, 1000);
+    const clock = setInterval(actualizarContador, 1000);
 
-    return function limpieza() {
-      clearInterval(reloj);
+    return function clear() {
+      clearInterval(clock);
     };
   });
 
   return (
     <div
-      className="flex flex-col md:flex-row justify-center items-center rounded-lg
-      backdrop-blur-md bg-opacity-100 relative overflow-hidden rounded-box
-      bg-cover bg-no-repeat text-center w-full h-full"
+      className="flex flex-col md:flex-row justify-center items-center bg-cover bg-no-repeat
+      w-full h-full"
       style={{
-        backgroundImage: `url(${Playa})`,
+        backgroundImage: `url(${Beach})`,
       }}
     >
-      <div className="flex flex-wrap justify-center items-center">
+      <div className="flex flex-wrap justify-center items-center backdrop-blur-md rounded-lg">
         <div className="relative">
           <svg className="-rotate-90 h-48 w-48">
             <circle
@@ -94,7 +93,7 @@ const Countdown = () => {
             className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
             flex flex-col items-center w-24 h-20"
           >
-            <span className="text-center">{dias}</span>
+            <span className="text-center">{days}</span>
             <span className="text-center">Dias</span>
           </div>
         </div>
@@ -122,7 +121,7 @@ const Countdown = () => {
             className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
             flex flex-col items-center w-24 h-20"
           >
-            <span className="text-center">{horas}</span>
+            <span className="text-center">{hours}</span>
             <span className="text-center">Horas</span>
           </div>
         </div>
@@ -150,7 +149,7 @@ const Countdown = () => {
             className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
             flex flex-col items-center w-24 h-20"
           >
-            <span className="text-center">{minutos}</span>
+            <span className="text-center">{minutes}</span>
             <span className="text-center">Minutos</span>
           </div>
         </div>
@@ -178,7 +177,7 @@ const Countdown = () => {
             className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
             flex flex-col items-center w-24 h-20"
           >
-            <span className="text-center">{segundos}</span>
+            <span className="text-center">{seconds}</span>
             <span className="text-center">Segundos</span>
           </div>
         </div>
