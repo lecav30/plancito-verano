29bdebe1caf2ddaa82283137244e2457f44a96fc
lecav30 <lecav30@outlook.com>
2024-02-17 19:59:16 -0500

    responsive countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,7 +1,10 @@
 import { useRef, useState, useEffect } from "react";
 import Beach from "../../images/playahd2.jpg";
+import useScreenWidth from "../../hooks/useScreenWidth";
 
 const Countdown = () => {
+  const screenWidth = useScreenWidth();
+
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
@@ -68,12 +71,16 @@ const Countdown = () => {
         backgroundImage: `url(${Beach})`,
       }}
     >
-      <h1 className="absolute text-6xl font-bold top-[10%]">
-        PLANCITO DE VERANO
+      <h1
+        className="absolute font-bold top-[10%] text-center text-5xl md:text-6xl 
+        leading-snug md:leading-normal"
+      >
+        PLANCITO <br className="md:hidden" />
+        DE <br className="md:hidden" />
+        VERANO
       </h1>
-
-      <div className="flex flex-wrap justify-center items-center backdrop-blur-md rounded-lg ">
-        <div className="relative ">
+      {screenWidth < 768 ? (
+        <div className="relative backdrop-blur-md rounded-lg mt-20">
           <svg className="-rotate-90 h-48 w-48">
             <circle
               r="70"
@@ -94,98 +101,135 @@ const Countdown = () => {
             ></circle>
           </svg>
           <div
-            className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
+            className="text-[#000000] absolute top-[70px] left-11 text-xl font-semibold
             flex flex-col items-center w-24 h-20"
           >
-            <span className="text-center">{days}</span>
-            <span className="text-center">Dias</span>
+            <div className="flex gap-2">
+              <span className="text-center">{days}</span>
+              <span className="text-center">Días</span>
+            </div>
+            <div className="flex gap-2">
+              <span className="text-center">{hours}</span>
+              <span className="text-center">Horas</span>
+            </div>
           </div>
         </div>
-        <div className="relative">
-          <svg className="-rotate-90 h-48 w-48">
-            <circle
-              r="70"
-              cx="90"
-              cy="90"
-              className="fill-transparent stroke-[#88adf1] stroke-[8px]"
-            ></circle>
-            <circle
-              r="70"
-              ref={hourCircle}
-              cx="90"
-              cy="90"
-              style={{
-                strokeDasharray: "451px",
-                strokeDashoffset: "451px",
-              }}
-              className="fill-transparent stroke-white stroke-[8px]"
-            ></circle>
-          </svg>
-          <div
-            className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
+      ) : (
+        <div className="flex flex-wrap justify-center items-center backdrop-blur-md rounded-lg">
+          <div className="relative">
+            <svg className="-rotate-90 h-48 w-48">
+              <circle
+                r="70"
+                cx="90"
+                cy="90"
+                className="fill-transparent stroke-[#88adf1] stroke-[8px]"
+              ></circle>
+              <circle
+                r="70"
+                ref={daysCircle}
+                cx="90"
+                cy="90"
+                style={{
+                  strokeDasharray: "440px",
+                  strokeDashoffset: "440px",
+                }}
+                className="fill-transparent stroke-white stroke-[8px]"
+              ></circle>
+            </svg>
+            <div
+              className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
             flex flex-col items-center w-24 h-20"
-          >
-            <span className="text-center">{hours}</span>
-            <span className="text-center">Horas</span>
+            >
+              <span className="text-center">{days}</span>
+              <span className="text-center">Dias</span>
+            </div>
           </div>
-        </div>
-        <div className="relative">
-          <svg className="-rotate-90 h-48 w-48">
-            <circle
-              r="70"
-              cx="90"
-              cy="90"
-              className="fill-transparent stroke-[#88adf1] stroke-[8px]"
-            ></circle>
-            <circle
-              r="70"
-              ref={minuteCircle}
-              cx="90"
-              cy="90"
-              style={{
-                strokeDasharray: "451px",
-                strokeDashoffset: "451px",
-              }}
-              className="fill-transparent stroke-white stroke-[8px]"
-            ></circle>
-          </svg>
-          <div
-            className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
+          <div className="relative">
+            <svg className="-rotate-90 h-48 w-48">
+              <circle
+                r="70"
+                cx="90"
+                cy="90"
+                className="fill-transparent stroke-[#88adf1] stroke-[8px]"
+              ></circle>
+              <circle
+                r="70"
+                ref={hourCircle}
+                cx="90"
+                cy="90"
+                style={{
+                  strokeDasharray: "451px",
+                  strokeDashoffset: "451px",
+                }}
+                className="fill-transparent stroke-white stroke-[8px]"
+              ></circle>
+            </svg>
+            <div
+              className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold
+              flex flex-col items-center w-24 h-20"
+            >
+              <span className="text-center">{hours}</span>
+              <span className="text-center">Horas</span>
+            </div>
+          </div>
+          <div className="relative">
+            <svg className="-rotate-90 h-48 w-48">
+              <circle
+                r="70"
+                cx="90"
+                cy="90"
+                className="fill-transparent stroke-[#88adf1] stroke-[8px]"
+              ></circle>
+              <circle
+                r="70"
+                ref={minuteCircle}
+                cx="90"
+                cy="90"
+                style={{
+                  strokeDasharray: "451px",
+                  strokeDashoffset: "451px",
+                }}
+                className="fill-transparent stroke-white stroke-[8px]"
+              ></circle>
+            </svg>
+            <div
+              className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
             flex flex-col items-center w-24 h-20"
-          >
-            <span className="text-center">{minutes}</span>
-            <span className="text-center">Minutos</span>
+            >
+              <span className="text-center">{minutes}</span>
+              <span className="text-center">Minutos</span>
+            </div>
           </div>
-        </div>
-        <div className="relative">
-          <svg className="-rotate-90 h-48 w-48">
-            <circle
-              r="70"
-              cx="90"
-              cy="90"
-              className="fill-transparent stroke-[#88adf1] stroke-[8px]"
-            ></circle>
-            <circle
-              r="70"
-              ref={secondCircle}
-              cx="90"
-              cy="90"
-              style={{
-                strokeDasharray: "451px",
-                strokeDashoffset: "451px",
-              }}
-              className="fill-transparent stroke-white stroke-[8px]"
-            ></circle>
-          </svg>
-          <div
-            className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
+          <div className="relative">
+            <svg className="-rotate-90 h-48 w-48">
+              <circle
+                r="70"
+                cx="90"
+                cy="90"
+                className="fill-transparent stroke-[#88adf1] stroke-[8px]"
+              ></circle>
+              <circle
+                r="70"
+                ref={secondCircle}
+                cx="90"
+                cy="90"
+                style={{
+                  strokeDasharray: "451px",
+                  strokeDashoffset: "451px",
+                }}
+                className="fill-transparent stroke-white stroke-[8px]"
+              ></circle>
+            </svg>
+            <div
+              className="text-[#000000] absolute top-16 left-11 text-2xl font-semibold 
             flex flex-col items-center w-24 h-20"
-          >
-            <span className="text-center">{seconds}</span>
-            <span className="text-center">Segundos</span>
+            >
+              <span className="text-center">{seconds}</span>
+              <span className="text-center">Segundos</span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
